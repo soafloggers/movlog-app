da9aee79408ccde7666b35e44cfda82faa614f3e
Yu-Shiang <z58085111@gmail.com>
2016-12-25 12:39:26 +0800

    dropmenu changes title

--- public/js/init.js
+++ public/js/init.js
@@ -1,4 +1,8 @@
 $(function(){
+  $(".dropdown-menu li a").click(function(){
+    $(this).closest(".dropdown, .dropup").find("span:first").text($(this).text());
+  });
+
   $(".location-btn").on("click", function(){
     find_rooms($(this).text());
   });
